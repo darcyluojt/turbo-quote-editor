6ac9072b9c1665f858e8339f8cbdec5b88678739
Darcy Luo <darcyluojt@gmail.com>
2025-02-05 16:58:43 +0000

    turbo frames

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -26,9 +26,9 @@ class QuotesTest < ApplicationSystemTestCase
 
   test "showing a Quote" do
     visit quotes_path
-    click_on @quote.Name
+    click_on @quote.name
 
-    assert_selector "h1", text: "Quotes"
+    assert_selector "h1", text: @quote.name
   end
 
   test "updating a Quote" do
@@ -38,14 +38,13 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Edit Quote"
     fill_in "Name", with: "Updated quote"
     click_on "Update Quote"
-    assert_selector "h1", text: "Quotes"
-    assert_text "Updated quote"
+    assert_selector "h1", text: "Updated quote"
   end
 
   test "destroying a Quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
-    click_on "Destroy", match: :first
-    assert_no_text @quote.Name
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
   end
 end
